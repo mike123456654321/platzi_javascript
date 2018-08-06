eb95cad62e8d70678e4ee6fc26daaf88b18f36bf
mike <miguelangelvilledadaf@gmail.com>
2018-08-06 13:31:29 -0500

    variables

---
+++ js/java.js
@@ -0,0 +1,6 @@
+let base = 5
+let altura = 7
+
+const triangleArea = (base, altura) => base*altura/2
+
+console.log('el are del triangulo es de base ${base} y altura ${altura} es: ${triangleArea(base, altura)}')
