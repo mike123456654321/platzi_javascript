2ab95f88095160a52bba970234a4df2d23c462a0
mike <miguelangelvilledadaf@gmail.com>
2018-08-08 18:09:12 -0500

    uso de string, contatenar variables, convertir arrays, metodos utiles

--- js/java.js
+++ js/java.js
@@ -8,6 +8,7 @@ const triangleArea = (base, altura) => base*altura/2
 console.log('el are del triangulo es de base ${base} y altura ${altura} es: ${triangleArea(base, altura)}')
 */
 
+/*
 const starwars = "star wars: el despertar de las fuerzas"
 const pgstarwars = 13
 
@@ -36,6 +37,60 @@ function whatchstarwar(name, age, whitchadulto = false)
 whatchstarwar (name1, age1)
 whatchstarwar (name2, age2, true)
 
+*/
+
+function platzom(str)
+{
+    let translation = str
+    //si la palabra ternima en "ar" se le quita los dos caracteres
+    if(str.toLowerCase().endsWith('ar'))
+        {
+            translation= str.slice(0, -2)
+        }
+
+    //si la palabra empieza con z se le añade "pe" al final
+    if(str.toLowerCase().startsWith('z'))
+       {
+       translation +='pe'
+       }
+
+    //si la palabra tiene mas de 10 letras se debe partir a la mitad y unir con un "-"
+    const length = translation.length
+    if(length>=10)
+       {
+       const firstHalf = translation.slice(0,Math.round(length/2))
+       const secontHalf = translation.slice(Math.round(length/2))
+       translation = (firstHalf+"-"+secontHalf)
+       }
+    //si la palabra es un palindromo ninguna regla anterior no cuentan
+
+    const reverse =(str) => str.split('').reverse().join('')
+
+    function misnMay(str)
+    {
+        const leng = str.length
+        let translation =''
+        let capitalize = true
+        for (let i = 0; i<length; i++)
+            {
+                const char = str.charAt(i)
+                translation += capitalize ? char.toUpperCase() : char.toLowerCase()
+                capitalize = !capitalize
+            }
+        return translation
+    }
+    if(str == reverse(str))
+       {
+       return misnMay(str)
+       }
+
+    return translation
+}
+console.log(platzom('progragar'))
+console.log(platzom('zorro'))
+console.log(platzom('zarpar'))
+console.log(platzom('abecedario'))
+console.log(platzom('sometemos'))
 
 
 
